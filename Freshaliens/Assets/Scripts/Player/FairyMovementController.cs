da11a3ae23be1a18be424c98b300de976f283be7
itsPeetah <pietro.g.moroni@gmail.com>
2022-11-16 12:12:29 +0100

    Added Fairy distance based return

--- Freshaliens/Assets/Scripts/Player/FairyMovementController.cs
+++ Freshaliens/Assets/Scripts/Player/FairyMovementController.cs
@@ -5,23 +5,41 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D)), RequireComponent(typeof(PlayerInputHandler))]
 public class FairyMovementController : MonoBehaviour
 {
-    [Header("Movement")]
+    private static FairyMovementController instance = null;
+    public static FairyMovementController Instance { get => instance; private set => instance = value; }
+
+    [Header("Player Controlled Movement")]
     [SerializeField] private float maxSpeed = 6f;
     [SerializeField] private float movementAcceleration = 5f;
     [SerializeField] private float movementDeceleration = 20f;
+
+    [Header("External Control")]
+    [SerializeField] private float maxDistanceBeforeReturn = 30f;
+    [SerializeField] private float returnDistance = 15f;
+    [SerializeField] private float returnAcceleration = 40f;
+    [SerializeField] private float maxReturnSpeed = 10f;
+
     // State
     private Vector2 movementDirection = Vector2.zero;
     private Vector2 currentVelocity = Vector2.zero;
     private bool isMoving = false;
+    private bool isReturningToPlayer = false;
 
     // Components
-    private PlayerInputHandler input;
-    private Rigidbody2D rbody;
+    private PlayerInputHandler input = null;
+    private Rigidbody2D rbody = null;
+    private Transform ownTransform = null;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start()
     {
         input = GetComponent<PlayerInputHandler>();
         rbody = GetComponent<Rigidbody2D>();
+        ownTransform = transform;
     }
 
     private void Update()
@@ -31,17 +49,25 @@ public class FairyMovementController : MonoBehaviour
         isMoving = movementDirection != Vector2.zero;
 
         // Calculate acceleration based on wether the player wants to move or stop
-        float acceleration = movementAcceleration;
-        if (!isMoving) acceleration = movementDeceleration;
+        float acceleration = !isMoving ? movementDeceleration : movementAcceleration;
+        float currentMaxSpeed = maxSpeed;
+
+        // Should the fairy return towards the player?
+        float distanceFromPlayer = Vector2.Distance(ownTransform.position, PlayerMovementController.Instance.Position);
+        isReturningToPlayer = distanceFromPlayer >= maxDistanceBeforeReturn || (isReturningToPlayer && distanceFromPlayer > returnDistance);
+        if (isReturningToPlayer)
+        {
+            movementDirection = (PlayerMovementController.Instance.Position - ownTransform.position);
+            isMoving = true;
+            currentMaxSpeed = maxReturnSpeed;
+            acceleration = returnAcceleration;
+        }
 
         // Calculate change in velocity
         currentVelocity = rbody.velocity;
         Vector2 targetVelocity = movementDirection.normalized * maxSpeed;
         Vector2 deltaVelocity = targetVelocity - currentVelocity;
-        float dampen = Mathf.Clamp01(acceleration * Time.deltaTime / maxSpeed);
+        float dampen = Mathf.Clamp01(acceleration * Time.deltaTime / currentMaxSpeed);
         rbody.velocity = currentVelocity + dampen * deltaVelocity;
-        
     }
-
-
 }
