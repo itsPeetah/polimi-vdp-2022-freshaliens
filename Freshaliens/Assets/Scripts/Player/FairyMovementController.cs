9fa0c0a2252b123b756a850d8ca3954ef5093d59
itsPeetah <pietro.g.moroni@gmail.com>
2022-11-28 19:09:03 +0100

    Made fairy lock onto targets
    It has priority over returning

--- Freshaliens/Assets/Scripts/Player/FairyMovementController.cs
+++ Freshaliens/Assets/Scripts/Player/FairyMovementController.cs
@@ -23,16 +23,29 @@ namespace Freshaliens.Player.Components
         [SerializeField] private float returnAcceleration = 40f;
         [SerializeField] private float maxReturnSpeed = 10f;
 
+        [Header("World Interaction")]
+        [SerializeField] private float stunDuration = 3f;
+
         // State
         private Vector2 movementDirection = Vector2.zero;
         private Vector2 currentVelocity = Vector2.zero;
+        private Transform lockOnTarget = null;
+        private float stunTimer = 0;
         private bool isMoving = false;
         private bool isReturningToPlayer = false;
+        private bool isLockingOnTarget = true;
+        private bool lockOnTargetAssigned = false;
 
         // Components
         private PlayerInputHandler input = null;
         private Rigidbody2D rbody = null;
         private Transform ownTransform = null;
+
+        // Properties
+        public bool IsMoving => isMoving;
+        public bool IsReturning => isReturningToPlayer;
+        public bool IsStunned => stunTimer > 0;
+        public bool IsLockingOnTarget => isLockingOnTarget;
 
         private void Awake()
         {
@@ -59,12 +72,28 @@ namespace Freshaliens.Player.Components
             // Should the fairy return towards the player?
             float distanceFromPlayer = Vector2.Distance(ownTransform.position, PlayerMovementController.Instance.Position);
             isReturningToPlayer = distanceFromPlayer >= maxDistanceBeforeReturn || (isReturningToPlayer && distanceFromPlayer > returnDistance);
-            if (isReturningToPlayer)
+
+            // Is the fairy locking onto a target?
+            isLockingOnTarget = lockOnTargetAssigned && Vector2.Distance(ownTransform.position, lockOnTarget.position) > 0.01f;
+            if (!isLockingOnTarget) {
+                lockOnTarget = null;
+                lockOnTargetAssigned = false;
+            }
+
+            if (isReturningToPlayer || isLockingOnTarget)
             {
-                movementDirection = (PlayerMovementController.Instance.Position - ownTransform.position);
+                movementDirection = isLockingOnTarget ? (lockOnTarget.position - ownTransform.position) : (PlayerMovementController.Instance.Position - ownTransform.position);
                 isMoving = true;
                 currentMaxSpeed = maxReturnSpeed;
                 acceleration = returnAcceleration;
+            }
+            else if (stunTimer > 0)
+            {
+                movementDirection = Vector3.zero;
+                isMoving = false;
+                currentMaxSpeed = 0;
+                acceleration = 0;
+                stunTimer -= Time.deltaTime;
             }
 
             // Calculate change in velocity
@@ -73,6 +102,23 @@ namespace Freshaliens.Player.Components
             Vector2 deltaVelocity = targetVelocity - currentVelocity;
             float dampen = Mathf.Clamp01(acceleration * Time.deltaTime / currentMaxSpeed);
             rbody.velocity = currentVelocity + dampen * deltaVelocity;
+        }
+
+        private void PerformMovement(Vector2 direction, float acceleration, float maxSpeed) {
+            currentVelocity = rbody.velocity;
+            Vector2 targetVelocity = movementDirection.normalized * maxSpeed;
+            Vector2 deltaVelocity = targetVelocity - currentVelocity;
+            float dampen = Mathf.Clamp01(acceleration * Time.deltaTime / maxSpeed);
+            rbody.velocity = currentVelocity + dampen * deltaVelocity;
+        }
+
+        public void Stun(float extraTime = 0) {
+            stunTimer = stunDuration + extraTime;
+        }
+
+        public void SetLockOnTarget(Transform target) {
+            lockOnTarget = target;
+            lockOnTargetAssigned = true;
         }
     }
 }
