f6f2818a0400324f1f793894b06fbf2c9caa8424
FraLeone837 <10572353@polimi.it>
2022-11-28 15:30:27 +0100

    Removed comments from Interactable

--- Freshaliens/Assets/Scripts/LevelObjects/Interactable/Interactable.cs
+++ Freshaliens/Assets/Scripts/LevelObjects/Interactable/Interactable.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
-    // protected const int FAIRYLAYER = 8;
-    //
-    // protected bool CheckLayer(int layer)
-    // {
-    //     return layer == FAIRYLAYER;
-    // }
-
     [SerializeField] protected bool storable = true;
     public bool ShouldBeStored => storable;
     
@@ -23,29 +16,4 @@ public abstract class Interactable : MonoBehaviour
     
     public virtual void OnFairyStay() { }
     
-    // private void OnTriggerEnter2D(Collider2D collision)
-    // {
-    //     if (CheckLayer(collision.gameObject.layer) && collision.gameObject.TryGetComponent(out FairyInteractionController fairy))
-    //     {
-    //         fairy.SetStoredInteractable(this);
-    //         OnFairyEnter();
-    //     }
-    // }
-    //
-    // private void OnTriggerStay2D(Collider2D collision)
-    // {
-    //     if (CheckLayer(collision.gameObject.layer) && collision.gameObject.TryGetComponent(out FairyInteractionController fairy))
-    //     {
-    //         OnFairyStay();
-    //     }
-    // }
-    //
-    // private void OnTriggerExit2D(Collider2D collision)
-    // {
-    //     if (CheckLayer(collision.gameObject.layer) && collision.gameObject.TryGetComponent(out FairyInteractionController fairy))
-    //     {
-    //         fairy.SetStoredInteractable(null);
-    //         OnFairyExit();
-    //     }
-    // }
 }
