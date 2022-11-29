702a49e73cfda4c26f1c17b4bf241c86e531dc6d
itsPeetah <pietro.g.moroni@gmail.com>
2022-11-29 11:24:35 +0100

    Update LevelSelectionCamera.cs

--- Freshaliens/Assets/Scripts/Level Selection/Components/LevelSelectionCamera.cs
+++ Freshaliens/Assets/Scripts/Level Selection/Components/LevelSelectionCamera.cs
@@ -6,6 +6,26 @@ namespace Freshaliens.LevelSelection.Components
 {
     public class LevelSelectionCamera : MonoBehaviour
     {
+        [SerializeField] private BoxCollider2D boundCollider = null;
         [SerializeField] private Transform target = null;
+        private Transform ownTransform = null;
+        private Camera cam = null;
+        private Bounds bounds;
+
+        private void Start()
+        {
+            ownTransform = transform;
+            cam = GetComponent<Camera>();
+            bounds = boundCollider.bounds;
+        }
+
+        private void Update()
+        {
+            float halfViewportHeightWS = cam.orthographicSize;
+            float halfViewportWidthWS = cam.orthographicSize * cam.aspect;
+
+
+            
+        }
     }
 }
