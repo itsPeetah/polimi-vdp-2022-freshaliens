09382a74d579e5c7437efd66717a85b0a029f5e4
itsPeetah <pietro.g.moroni@gmail.com>
2022-11-29 10:23:29 +0100

    Level Selection Camera Script

---
+++ Freshaliens/Assets/Scripts/Level Selection/Components/LevelSelectionCamera.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Freshaliens.LevelSelection.Components
+{
+    public class LevelSelectionCamera : MonoBehaviour
+    {
+        [SerializeField] private Transform target = null;
+    }
+}
