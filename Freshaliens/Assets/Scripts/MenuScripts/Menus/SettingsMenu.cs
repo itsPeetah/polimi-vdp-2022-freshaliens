18e3d090fecb0e73083f34384c2e02152b4a4c40
itsPeetah <pietro.g.moroni@gmail.com>
2022-11-30 15:16:13 +0100

    Removed DataManagement code space

--- Freshaliens/Assets/Scripts/MenuScripts/Menus/SettingsMenu.cs
+++ Freshaliens/Assets/Scripts/MenuScripts/Menus/SettingsMenu.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using GameManagement.Data;
 using GameManagement.Audio;
 
 namespace MenuManagement
