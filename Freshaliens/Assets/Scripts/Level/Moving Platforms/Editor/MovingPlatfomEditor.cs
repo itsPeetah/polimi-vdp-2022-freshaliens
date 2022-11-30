1467436cc062ff009f33c2c82a612bcaf7d52054
itsPeetah <pietro.g.moroni@gmail.com>
2022-11-30 16:41:27 +0100

    Added basic moving platform

---
+++ Freshaliens/Assets/Scripts/Level/Moving Platforms/Editor/MovingPlatfomEditor.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+using Freshaliens.Level.Components;
+namespace Freshaliens.Level.Editor
+{
+    [CustomEditor(typeof(MovingPlatform))]
+    public class MovingPlatfomEditor : UnityEditor.Editor
+    {
+        private MovingPlatform platform;
+
+        private void OnEnable()
+        {
+            platform = target as MovingPlatform;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+        }
+
+        private void OnSceneGUI()
+        {
+            EditorGUI.BeginChangeCheck();
+            Vector3 startingPosition = Handles.PositionHandle(platform.StartPosition, Quaternion.identity);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(platform, "Change Look At Target Position");
+                platform.StartPosition = startingPosition;
+            }
+
+            EditorGUI.BeginChangeCheck();
+            Vector3 endPosition = Handles.PositionHandle(platform.EndPosition, Quaternion.identity);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(platform, "Change Look At Target Position");
+                platform.EndPosition = endPosition;
+            }
+        }
+    }
+
+}
