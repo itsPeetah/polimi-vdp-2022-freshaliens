09117b4e7e395f38e8153fd1778b640f89f26bcc
itsPeetah <pietro.g.moroni@gmail.com>
2022-12-02 12:34:35 +0100

    Update Checkpoint.cs
    - Final checkpoint
    - fixed OnTriggerEnter

--- Freshaliens/Assets/Scripts/Level/Checkpoint/Checkpoint.cs
+++ Freshaliens/Assets/Scripts/Level/Checkpoint/Checkpoint.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using MenuManagement;
 
 namespace Freshaliens.Level.Components
 {
@@ -20,6 +20,7 @@ namespace Freshaliens.Level.Components
 
         [Header("Interaction")]
         [SerializeField] private bool isStartingCheckpoint = false;
+        [SerializeField] private bool isFinalCheckpoint = false;
         [SerializeField,Tooltip("Should the checkpoint be activated every time the player triggers it?")] private bool allowMultipleActivations = false;
 
         private bool hasBeenActivated = false;
@@ -27,9 +28,18 @@ namespace Freshaliens.Level.Components
         private void Start()
         {
             Setup();
-            if (isStartingCheckpoint) {
+            if (isStartingCheckpoint)
+            {
                 lastActiveCheckpoint = this;
+#if UNITY_EDITOR
+                if (isFinalCheckpoint) Debug.LogWarning("The starting checkpoint is also the final checkpoint...WTF?");
+#endif
             }
+
+            if (isFinalCheckpoint) {
+                allowMultipleActivations = false;
+            }
+            
         }
 
         private void Reset()
@@ -56,10 +66,17 @@ namespace Freshaliens.Level.Components
             }
         }
 
-        private void OnTriggerEnter(Collider other)
+        private void OnTriggerEnter2D(Collider2D other)
         {
             if (hasBeenActivated && !allowMultipleActivations) return;
             // HACK For now I'll just make a new layer that only collides with player 1
+
+            if (isFinalCheckpoint) {
+                MenuManager.Instance.OpenMenu(MenuManager.Instance.levelCompletedScreen); // TODO Move this to game manager script
+                return;
+            }
+
+            // Not final checkpoint -> update last available checkpoint
             lastActiveCheckpoint = this;
             hasBeenActivated = true;
         }
