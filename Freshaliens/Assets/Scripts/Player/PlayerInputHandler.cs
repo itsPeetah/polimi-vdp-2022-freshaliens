c7e9541a211d7216e25604a7d33b8b7079257e76
itsPeetah <pietro.g.moroni@gmail.com>
2022-12-04 18:11:50 +0100

    Stopped input while paused

--- Freshaliens/Assets/Scripts/Player/PlayerInputHandler.cs
+++ Freshaliens/Assets/Scripts/Player/PlayerInputHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MenuManagement;
 
 namespace Freshaliens.Player.Components
 {
@@ -14,23 +15,29 @@ namespace Freshaliens.Player.Components
         [SerializeField] private string jumpAxis = "Jump";
         [SerializeField] private string actionAxis = "Fire";
 
+        private bool IsPaused => PauseMenu.GameIsPaused;
+
         public bool GetJumpInput()
         {
+            if (IsPaused) return false;
             return Input.GetButtonDown(jumpAxis);
         }
 
         public float GetHorizontal()
         {
+            if (IsPaused) return 0;
             return Input.GetAxisRaw(horizontalAxis);
         }
 
         public float GetVertical()
         {
+            if (IsPaused) return 0;
             return Input.GetAxisRaw(verticalAxis);
         }
 
         public bool GetActionInput()
         {
+            if (IsPaused) return false;
             return Input.GetButtonDown(actionAxis);
         }
 
