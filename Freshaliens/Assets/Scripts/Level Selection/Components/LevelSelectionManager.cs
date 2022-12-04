8a5e90df12cfa890bfa6fed0d7641c88cdfb4bf2
itsPeetah <pietro.g.moroni@gmail.com>
2022-12-05 00:20:34 +0100

    Update LevelSelectionManager.cs

--- Freshaliens/Assets/Scripts/Level Selection/Components/LevelSelectionManager.cs
+++ Freshaliens/Assets/Scripts/Level Selection/Components/LevelSelectionManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.SceneManagement;
-
+using MenuManagement;
 namespace Freshaliens.LevelSelection.Components
 {
     /// <summary>
@@ -48,7 +48,7 @@ namespace Freshaliens.LevelSelection.Components
         private void Start()
         {
             currentlySelectedLevel = PlayerData.Instance.LastLevelSelected;
-
+            PauseMenu.GameIsPaused = false;
             InstantiateMapLines();
             UpdateMapLineState();
 
@@ -86,6 +86,7 @@ namespace Freshaliens.LevelSelection.Components
         private void SelectLevel(int index)
         {
             Time.timeScale = 1;
+            
             int prevSelectedLevel = currentlySelectedLevel;
             int levelToSelect = Mathf.Abs(index) % levels.Length;
 
