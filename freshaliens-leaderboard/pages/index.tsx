ea93f7de37245f09927a5ea2d97aa265cf6db604
itspeetah <pietro.g.moroni@gmail.com>
2022-12-05 15:10:13 +0100

    Fixed

--- freshaliens-leaderboard/pages/index.tsx
+++ freshaliens-leaderboard/pages/index.tsx
@@ -2,8 +2,14 @@ import getConfig from "next/config";
 
 import Layout from "@/components/Layout";
 import { useEffect, useState } from "react";
-import { LeaderboardEntry, Score } from "src/types";
-import { get, getDatabase, ref } from "firebase/database";
+import { Leaderboard, LeaderboardEntry } from "src/types";
+import {
+  DataSnapshot,
+  get,
+  getDatabase,
+  onValue,
+  ref,
+} from "firebase/database";
 import { initializeApp } from "firebase/app";
 import { dbroot } from "src/realtimeDatabase";
 import { firebaseConfig } from "src/firebaseConfig";
@@ -14,87 +20,112 @@ const apiURL =
   "https://vdp22-freshaliens-leaderboard.vercel.app/api/leaderboard";
 
 const app = initializeApp(firebaseConfig);
-const db = getDatabase();
+const db = getDatabase(app);
 
 const Home = () => {
-  const [level, setLevel] = useState(1);
-  const [leaderboardData, setLeaderboardData] = useState({});
-  const [child, setChild] = useState<JSX.Element[]>([]);
+  const [selectedLevel, setLevel] = useState("1");
+  const [leaderboardData, setLeaderboardData] = useState<Leaderboard>([]);
+  const [scoreboardEntries, setScoreboardEntries] = useState<JSX.Element[]>([]);
 
-  const buildLeaderboard = (level: number): JSX.Element[] => {
+  const buildLeaderboard = (data: Leaderboard) => {
     const children: JSX.Element[] = [];
-    for (const [key, value] of Object.entries(leaderboardData)) {
-      const entry = value as LeaderboardEntry;
-      for (let s in entry.times) {
-        if (s === level.toString()) {
-          children.push(
-            <div className="flex flex-row justify-between border-b-2 border-gray-200">
-              <span>{key}</span>
-              <span>{entry.times[s]}</span>
-            </div>
-          );
-        }
+    for (let name in data) {
+      for (let lvl in (data as any)[name]) {
+        children.push(
+          <ScoreboardEntry
+            key={`Leaderboard_${name}_${lvl}`}
+            name={name}
+            time={(data as any)[name][lvl]}
+            show={lvl.toString() === selectedLevel.toString()}
+          />
+        );
       }
     }
-    return children;
+
+    setScoreboardEntries(children);
   };
 
   useEffect(() => {
-    get(ref(db, dbroot)).then((snap) => {
-      setLeaderboardData(snap.val());
+    onValue(ref(db, `${dbroot}`), (snapshot: DataSnapshot) => {
+      setLeaderboardData(snapshot.val() as Leaderboard);
+      buildLeaderboard(snapshot.val() as Leaderboard);
     });
-  }, [level]);
+  }, []);
 
   useEffect(() => {
-    const c = buildLeaderboard(level);
-    setChild(c);
-  }, [leaderboardData]);
+    buildLeaderboard(leaderboardData);
+  }, [selectedLevel]);
 
   return (
     <Layout>
       <div className="w-full h-screen | flex flex-col items-center | p-2 | bg-slate-900 text-white">
         <h1 className="text-6xl my-10">Freshaliens Leaderboard</h1>
         <div className="flex flex-row">
-          <button
-            className={
-              "p-2 text-yellow-400 border-2 border-current rounded-md" +
-              (level === 1 ? " text-black bg-yellow-400" : "")
-            }
-            type="button"
-            onClick={() => setLevel(1)}
-          >
-            Level 1
-          </button>
-          <button
-            className={
-              "p-2 text-yellow-400 border-2 border-current rounded-md" +
-              (level === 2 ? " text-black bg-yellow-400" : "")
-            }
-            type="button"
-            onClick={() => setLevel(2)}
-          >
-            Level 2
-          </button>
-          <button
-            className={
-              "p-2 text-yellow-400 border-2 border-current rounded-md" +
-              (level === 3 ? " text-black bg-yellow-400" : "")
-            }
-            type="button"
-            onClick={() => setLevel(3)}
-          >
-            Level 3
-          </button>
+          <LevelButton
+            level="1"
+            currentLevel={selectedLevel}
+            onClick={() => setLevel("1")}
+          />
+          <LevelButton
+            level="2"
+            currentLevel={selectedLevel}
+            onClick={() => setLevel("2")}
+          />
+          <LevelButton
+            level="3"
+            currentLevel={selectedLevel}
+            onClick={() => setLevel("3")}
+          />
         </div>
         <div className="w-[50%] mt-5 border-2 p-10">
           <div className="flex flex-row justify-between text-yellow-400 mb-2">
             <span>Name</span>
             <span>Time</span>
           </div>
-          {child}
+          {scoreboardEntries}
         </div>
       </div>
     </Layout>
+  );
+};
+
+interface LevelButtonProps {
+  level: string;
+  currentLevel: string;
+  onClick: () => void;
+}
+const LevelButton = (props: LevelButtonProps) => {
+  return (
+    <button
+      className={
+        "p-2 text-yellow-400 border-2 border-current rounded-md" +
+        (props.level === props.currentLevel ? " text-black bg-yellow-400" : "")
+      }
+      type="button"
+      onClick={props.onClick}
+    >
+      Level {props.level}
+    </button>
+  );
+};
+
+interface ScoreboardEntryProps {
+  name: string;
+  time: string;
+  show: boolean;
+}
+const ScoreboardEntry = (props: ScoreboardEntryProps) => {
+  return (
+    <div
+      className={
+        props.show
+          ? "flex flex-row justify-between border-b-2 border-gray-200"
+          : "hidden"
+      }
+    >
+      <span>{props.name}</span>
+      <span>{props.time}</span>
+    </div>
   );
 };
 
