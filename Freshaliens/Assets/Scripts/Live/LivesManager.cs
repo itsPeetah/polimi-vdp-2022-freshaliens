4383c6b42307e00f6fec7b50ceb6d118e8e8178d
itsPeetah <pietro.g.moroni@gmail.com>
2022-12-15 12:34:02 +0100

    Rewritten Lives Manager

--- Freshaliens/Assets/Scripts/Live/LivesManager.cs
+++ Freshaliens/Assets/Scripts/Live/LivesManager.cs
@@ -9,105 +9,22 @@ using Freshaliens.Management;
 
 public class LivesManager : MonoBehaviour
 {
-    private PlayerMovementController ninja;
-    
-    [SerializeField] private int initialNumberOfLives = 3;
     [SerializeField] private LayerMask hitLayers = -1;
     [SerializeField] private int deathLayer = 16;
-    
-    // STATE
-    public static int numberOfLives = 3; // UGLY, I wanted a Singleton but it's not possible...
-    private bool hit = false;
-    private bool _alreadyHit = false;
-    
-    void Start()
-    {
-        ninja = PlayerMovementController.Instance;
-        numberOfLives = initialNumberOfLives;
-        
-    }
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
         int layer = collision.gameObject.layer;
 
         if (( (1 << layer) & hitLayers) != 0) {
-            PlayerHit(false);
+            HitPlayer(false);
         }
-    }
-    
-    private void PlayerHit(bool mustRespawn)
-    {
-        numberOfLives--;
-        LevelManager.Instance.DamagePlayer();
-        if (numberOfLives == 0)
-        {
-            PlayerDeath();
-            return;
-        }
-
-        if (mustRespawn)
-        {
-            Respawn(Checkpoint.LastActiveCheckpoint.RespawnPosition);
-        }
-    }
-    
-    private void PlayerDeath()
-    {
-        Respawn(Checkpoint.StartingCheckpoint.RespawnPosition);
-        Checkpoint.ResetStartingCheckpoint();
-        numberOfLives = initialNumberOfLives;
-    }
-    
-    private void Respawn(Vector3 position)
-    {
-        ninja.transform.position = position;
-        FairyMovementController.Instance.RespawnWithNinja(position);
-    }
-    
-    private void HitByEnemy()
-    {
-        if (_alreadyHit)
-        {
-            _alreadyHit = false;
-            return;
-        }
-
-        _alreadyHit = true;
-        PlayerHit(false);
-        StartCoroutine(ResetAlreadyHit());
-    }
-    
-    IEnumerator ResetAlreadyHit()
-    {
-        yield return new WaitForSeconds(0.5f);
-        _alreadyHit = false;
-        yield return null;
     }
 
-    // For external usage
-    public void HitPlayer(bool isEnemy)
-    {
-        if (isEnemy)
-        {
-            HitByEnemy();
-        }
-        else
-        {
-            PlayerHit(false);
-        }
+    public void HitPlayer(bool triggerRespawn = false) {
+        LevelManager.Instance.DamagePlayer(skipInvulnerableCheck: triggerRespawn);
+        if (triggerRespawn && !LevelManager.Instance.GameOver) LevelManager.Instance.RespawnPlayer();
     }
-    
-    public void HitAndRespawn()
-    {
-        PlayerHit(true);
-    }
-    
-    public void DeathPLayer()
-    {
-        PlayerDeath();
-    }
-
     
 }
         
