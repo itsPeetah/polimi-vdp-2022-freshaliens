6d0157ecee0e781b06c641b621354d450f32c374
itsPeetah <pietro.g.moroni@gmail.com>
2022-12-21 16:04:22 +0100

    Added time submission to level completed UI

--- Freshaliens/Assets/Scripts/UI/LevelCompleteHUD.cs
+++ Freshaliens/Assets/Scripts/UI/LevelCompleteHUD.cs
@@ -1,7 +1,11 @@
+using System.Collections;
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
+using UnityEngine.Networking;
+using TMPro;
 using Freshaliens.Management;
+using Freshaliens.Social;
+
 namespace Freshaliens.UI
 {
     public class LevelCompleteHUD : UIScreen
@@ -10,6 +14,9 @@ namespace Freshaliens.UI
         [SerializeField] private TextMeshProUGUI timeLabel = null;
         [SerializeField] private TMP_InputField nicknameField = null;
         [SerializeField] private Button submitButton = null;
+        [SerializeField] private Button continueButton = null, restartButton = null;
+
+        private bool isUploadingData = false;
 
         protected override void Start()
         {
@@ -24,6 +31,8 @@ namespace Freshaliens.UI
         private void Update()
         {
             submitButton.interactable = nicknameField.text.Length >= 1;
+            continueButton.interactable = !isUploadingData;
+            restartButton.interactable = !isUploadingData;
         }
 
         public void OnContinuePress() {
@@ -38,7 +47,31 @@ namespace Freshaliens.UI
             if (nicknameField.text.Length < 1) return;
             string playerName = nicknameField.text;
             PlayerData.Instance.LeaderboardName = playerName;
-            // TODO Submit time
+            string playerTime = LevelManager.Instance.CurrentLevelTimerAsString;
+            int currLevel = LevelManager.Instance.CurrentLevel;
+            PostTime(playerName, playerTime, currLevel);
+        }
+
+        public void PostTime(string name, string time, int level)
+        {
+            Debug.Log("Posting time as " + name);
+            WWWForm form = new WWWForm();
+            form.AddField("name", name);
+            form.AddField("time", time);
+            form.AddField("level", level);
+            StartCoroutine(SendPost(form));
+        }
+
+        private IEnumerator SendPost(/*string payload*/WWWForm form)
+        {
+            isUploadingData = true;
+            UnityWebRequest www = UnityWebRequest.Post(Leaderboard.API_URL, /*payload*/ form);
+            yield return www.SendWebRequest();
+            Debug.Log(www.result);
+            Debug.Log("Posted: " + form);
+            www.Dispose();
+            Debug.Log("Disposed of www");
+            isUploadingData = false;
         }
     }
 }
